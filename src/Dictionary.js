a91da205eb9edbd14a2776733448b19d0b69fafe
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-07-13 11:54:26 -0400

    added documentation url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,7 +12,7 @@ export default function Dictionary() {
     alert(`Searching for ${keyword}`);
     let key = "43fa5d86069t4dbb87a934b227c8ob50";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
-
+    //documentation: https://www.shecodes.io/learn/apis/dictionary
     axios.get(apiUrl).then(handleResponse);
   }
   function handlekKeywordChange(event) {
