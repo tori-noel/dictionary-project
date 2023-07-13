0f39b180a98b0b474afdacebbd8faa3d361bfd8f
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-07-13 11:34:15 -0400

    created basic search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,6 +4,7 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
+
     alert(`Searching for ${keyword}`);
   }
   function handlekKeywordChange(event) {
