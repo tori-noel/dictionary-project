1a4a70ffbcfbe2c36aa37071a63d8ce72d716bb6
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-07-14 15:55:12 -0400

    added phonetic element to definition

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,7 @@ export default function Dictionary() {
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
     let key = "43fa5d86069t4dbb87a934b227c8ob50";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     //documentation: https://www.shecodes.io/learn/apis/dictionary
--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,7 @@ export default function Results(props) {
     return (
       <div className="results">
         <h2> {props.results.word} </h2>
+        <h3> {props.results.phonetic}</h3>
         {props.results.meanings.map(function (meanings, index) {
           return (
             <div key={index}>
