4d861c25a2765580facbc911f61d765a7d662cc8
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-07-20 11:08:49 -0400

    tried trouble shooting the pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
@@ -15,18 +16,18 @@ export default function Dictionary(props) {
     console.log(response);
   }
   function search() {
-    let key = "43fa5d86069t4dbb87a934b227c8ob50";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    const apiKey = "43fa5d86069t4dbb87a934b227c8ob50";
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     //documentation: https://www.shecodes.io/learn/apis/dictionary
     axios.get(apiUrl).then(handleResponse);
 
-    let pexelsApiKey =
+    const pexelsApiKey =
       "hRAT1yrOmcA1VHnv2f6p6fcCXHML02yvjoBm5gCRAGvKbuP1l2NMQt38";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
     axios
       .get(pexelsApiUrl, {
-        header: { Authorization: `Bearer ${pexelsApiKey}` },
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
       })
       .then(handlePexelsResponse);
   }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div className="Synonyms">
-        <ul Classname="Synonyms">
+        <ul className="Synonyms">
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
           })}
